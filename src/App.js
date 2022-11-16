699c043eee8a95b03bfff12e19ece69aae4c57fb
Asaad Budayr <asad_289@hotmail.com>
2022-11-16 21:07:23 +0300

    feat: add initial routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <Box width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
         <Navbar/>
+        <Routes>
+            <Route  path="/" element={<Home/>}/>
+        </Routes>
     </Box>
   )
 }
