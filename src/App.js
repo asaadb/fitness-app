c0ca7cac7516c6cf0e4a9905c639aa0e92fa13eb
Asaad Budayr <asad_289@hotmail.com>
2022-11-16 20:49:28 +0300

    feat: create Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: { xl: '1488px'}}} m="auto">
+        <Navbar/>
+    </Box>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactDoM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = React.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />)
+root.render(
+        <App />
+);
