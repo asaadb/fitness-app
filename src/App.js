9b60e76886ffbf9fed2c8455bb13a13e4ad2d09b
Asaad Budayr <asad_289@hotmail.com>
2022-11-27 22:28:59 +0300

    feat: display exercise detail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar/>
         <Routes>
             <Route  path="/" element={<Home/>}/>
+            <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
         </Routes>
     </Box>
   )
