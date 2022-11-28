6d5aa40f9322f59928c2538db3e356efd9e7ebb1
Asaad Budayr <asad_289@hotmail.com>
2022-11-28 21:07:50 +0300

    feat: add loader

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import HorizontalScrollbar from '../components/HorizontalScrollbar';
+import Loader from './Loader';
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentMuscleExercises }) => {
   return (
     <Box sx={{ mt: { lg: '100px', xs: '0'}}}>
       <Typography variant="h3">Exercises that target the same muscle group</Typography>
       <Stack direction="row" sx={{ p: '2', position: 'relative'}}>
-          {targetMuscleExercises.length && <HorizontalScrollbar data={targetMuscleExercises} />}
+          {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader/>}
       </Stack>
       <Typography variant="h3">Exercises that use the same equipment</Typography>
       <Stack direction="row" sx={{ p: '2', position: 'relative'}}>
-          {equipmentMuscleExercises.length && <HorizontalScrollbar data={equipmentMuscleExercises} />}
+          {equipmentMuscleExercises.length ? <HorizontalScrollbar data={equipmentMuscleExercises} /> : <Loader/>}
       </Stack>
 
     </Box>
