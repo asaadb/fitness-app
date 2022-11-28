d9073e9a6e0dea3727c317669743a7603885991f
Asaad Budayr <asad_289@hotmail.com>
2022-11-28 21:11:57 +0300

    style: add margin bottom

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -6,11 +6,11 @@ import Loader from './Loader';
 const SimilarExercises = ({ targetMuscleExercises, equipmentMuscleExercises }) => {
   return (
     <Box sx={{ mt: { lg: '100px', xs: '0'}}}>
-      <Typography variant="h3">Exercises that target the same muscle group</Typography>
+      <Typography variant="h3" mb={5}>Exercises that target the same muscle group</Typography>
       <Stack direction="row" sx={{ p: '2', position: 'relative'}}>
           {targetMuscleExercises.length ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader/>}
       </Stack>
-      <Typography variant="h3">Exercises that use the same equipment</Typography>
+      <Typography variant="h3" mb={5}>Exercises that use the same equipment</Typography>
       <Stack direction="row" sx={{ p: '2', position: 'relative'}}>
           {equipmentMuscleExercises.length ? <HorizontalScrollbar data={equipmentMuscleExercises} /> : <Loader/>}
       </Stack>
